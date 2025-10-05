196349a4a4d5bc8e1d268228a8e17aced7f0cbeb
Ueno1122345 <k4720741@kadai.jp>
2025-10-05 11:13:36 +0900

    復旧

--- my-app/src/api/state.js
+++
@@ -1,5 +0,0 @@
-import { api } from "./client.js";
-export async function getState({ month, userId }) {
-  const { data } = await api.get("/api/state", { params: { month, user_id: userId } });
-  return data; // { character, goals, calendar, entries }
-}
